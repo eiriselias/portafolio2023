d57237f22130e8f7daced4ed551c91557ab519fb
Elias Salazar <eiriselias@gmail.com>
2023-08-07 19:14:05 -0500

    se termino el home se agrego video de fondo y una selfie ademas se creo el componente skills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavBarComponent
+    NavBarComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
